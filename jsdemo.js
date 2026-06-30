cd2e14917019d7ff1d34be853edbedaae52b421f
hiruydagnew <hiruydagnew@gmail.com>
2026-06-30 14:22:13 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,4 +1,7 @@
 console.log("This is loaded from the external JS");
-console.log("Name: Hiruy Dagnew");
-console.log("School: CPCC");
-console.log("Hobby: Playing soccer");
+var name = "Hiruy Dagnew";
+var school = "CPCC";
+let hobby = "Playing Soccer";
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Hobby: ${hobby}`);
