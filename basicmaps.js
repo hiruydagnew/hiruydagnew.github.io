4aed0bd0a29039fe58b289020031f84f50af7ca2
hiruydagnew <hiruydagnew@gmail.com>
2026-07-07 14:20:16 -0400

    Update basicmaps.js

--- basicmaps.js
+++ basicmaps.js
@@ -1,6 +1,6 @@
 //35.2271° N latitude and -80.8431° W longitude
 var charlotte = [35.2271, -80.8431];
-var cpcc = [35.21, -80.83];
+var cpcc = [35.2176665, -80.831473];
 var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
