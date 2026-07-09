898c7a59eaaba30152d5e98be381b581d7a7b8aa
hiruydagnew <hiruydagnew@gmail.com>
2026-07-09 14:08:35 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -1,8 +1,15 @@
 //latitude of 9.1450° N and a longitude of 40.4897° E. T
+//12.03167° N latitude and 39.04111° E longitude
+//12.6074° N, 37.4700° E
+//14.12083° N, 38.72778° E
 var coordinates = [9.1450, 40.4897];
+var place1 = [12.03167, 39.04111];
+var place2 = [12.6074, 37.4700];
+var place3 = [14.12083, 38.72778];
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Ethiopia!</p>").addTo(map);
+var marker1 = L.circle(place1,{color: "black", fillColor: "#c96979", radius: 80}).addTo(map);
